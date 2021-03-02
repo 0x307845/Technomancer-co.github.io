45a5cfe7253194fcf8b9b8d36d26feebef483cbf
0xE <49166384+0x307845@users.noreply.github.com>
2021-03-02 19:16:19 +0000

    Update encrypt.js

--- js/encrypt.js
+++ js/encrypt.js
@@ -11,7 +11,7 @@ function encrypt_invite(str) {
     y: 'l', z: 'm', 1: '9',
     2: '8', 3: '7', 4: '6',
     5: '5', 6: '4', 7: '3',
-    8: '2', 9: '1',
+    8: '2', 9: '1', $:'$',
   }
   str = str.toLowerCase();
   let decipher = '';
